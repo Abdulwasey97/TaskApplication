7a2e59a7454d16018e24df24bb8e5f912411770f
Abdulwasey97 <abdulwaseyjaved@yahoo.com>
2024-02-11 21:12:27 +0500

    some Changes

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -76,34 +76,4 @@
         </div>
     </div>
 </x-app-layout>
-
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<script>
-    $(document).ready(function () {
-        $('.task-status').change(function () {
-            var taskId = $(this).data('task-id');
-            var newStatus = $(this).val();
-            var messageContainer = $(this).siblings('.message-container');
-
-            // Send AJAX request to update task status
-            $.ajax({
-                type: 'POST',
-                url: '{{ route('update.task.status') }}',
-                data: {
-                    _token: '{{ csrf_token() }}',
-                    task_id: taskId,
-                    status: newStatus
-                },
-                success: function (response) {
-                    console.log(response);
-                    messageContainer.html('<div class="text-green-500">Status updated successfully</div>');
-                },
-                error: function (error) {
-                    console.error(error);
-                    messageContainer.html('<div class="text-red-500">Failed to update status</div>');
-                }
-            });
-        });
-    });
-</script>
+@include('javascript.javascript')
